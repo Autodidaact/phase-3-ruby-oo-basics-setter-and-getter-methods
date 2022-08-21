ede9f9c7d54b4675c25702a10574b6aa5a575475
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-21 14:57:47 +0000

    Modified dog.rb && person.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-
+    #Name setter method
+    def name=(name)
+        @name = name
+    end
+    def name 
+        @name
+    end
+    #Breed getter method
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
 end
+
+#instance
+baa = Dog.new
+baa.name = "Baa"
+baa.breed = "exortic"
+
+puts baa.name
+puts baa.breed
