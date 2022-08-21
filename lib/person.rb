47edc3e927cd72a6d21214eb4656fc81699b5a6b
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-21 14:56:37 +0000

    commit tree 59c1dc2022567f97e9503d4aba6b04bd6ddbbdde

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,34 @@
 class Person
+    #Name etter method
+    def name=(name)
+        @name = name
+    end
+    #Name getter method
+    def name
+        @name
+    end
 
+    #Job setter method
+    def job=(job)
+        @job = job
+    end
+    #Job getter method
+    def job
+        @job
+    end
 end
+
+#instances
+ada = Person.new
+ada.name = "Ada"
+ada.job = "physician"
+
+puts ada.job
+puts ada.name
+
+ib = Person.new
+ib.name = "Ib"
+ib.job = "Neurosurgeon"
+
+puts ib.name
+puts ib.job
